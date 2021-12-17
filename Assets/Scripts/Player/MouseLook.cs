ef5477039c103e731ebe7ded0bd3722a8d417eb8
wuzzardAN <50873815+wuzzardAN@users.noreply.github.com>
2021-12-17 12:59:26 +0300

    Rigidbody movement + grapling

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -6,23 +6,10 @@ namespace MultiFps.Player
 {
     public class MouseLook : MonoBehaviour
     {
-        [Range(50,500)]
-        [SerializeField] private float _sens;
-        [SerializeField] private Transform _characterTransform;
+        public Transform player;
 
-        private float _xRot;
-
-        private void Update()
-        {
-            float rotX = Input.GetAxis("Mouse X") * _sens * Time.deltaTime;
-            float rotY = Input.GetAxis("Mouse Y") * _sens * Time.deltaTime;
-
-            _xRot -= rotY;
-            _xRot = Mathf.Clamp(_xRot, -80, 80);
-
-            transform.localRotation = Quaternion.Euler(_xRot, 0f, 0f);
-
-            _characterTransform.Rotate(Vector3.up * rotX);
+        void Update() {
+            transform.position = player.transform.position;
         }
     }
 }
